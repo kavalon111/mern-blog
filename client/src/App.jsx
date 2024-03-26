205e0ef798c24679e3289e8072a3403f540404d1
kavalon111 <ameyakavalon111@gmail.com>
2024-03-26 13:39:47 +0530

    Saved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,29 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
+import Project from './pages/Project';
+
 export default function App() {
   return (
-    <h1>
-      Hello world!
-    </h1>
-  )
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
+}
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/project" element={<Project />} />
+    </Routes>
+  );
 }
