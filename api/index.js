fc04bb32f8a8fe2f7a6c09acf7323c3fed4f3897
kavalon111 <ameyakavalon111@gmail.com>
2024-04-04 00:42:15 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js"
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -14,8 +15,12 @@ mongoose
     console.log(err);
   });
 const app = express();
+
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server is running on post 3000");
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -4,6 +4,6 @@ import { test }from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.get('/test',test
-  );
+);
 
 export default router;
