4b2a8a29a49e069d1d77ef6a6f97361fcc734534
kavalon111 <ameyakavalon111@gmail.com>
2024-04-05 02:05:01 +0530

    create sigin api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -3,7 +3,9 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-export const signup = async (req, res,next) => {
+import jwt from "jsonwebtoken";
+
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   if (
     !username ||
@@ -13,12 +15,11 @@ export const signup = async (req, res,next) => {
     email === " " ||
     password === " "
   ) {
-   next(errorHandler(400, 'All Fields are required'));
+    next(errorHandler(400, "All Fields are required"));
   }
 
-
   // to hide the password from the database
-  const hashedPassword = bcryptjs.hashSync(password,10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
   const newUser = new User({
     username,
     email,
@@ -27,6 +28,33 @@ export const signup = async (req, res,next) => {
   try {
     await newUser.save();
     res.json("signup successful");
+  } catch (error) {
+    next(error);
+  }
+};
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie('access_token', token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
