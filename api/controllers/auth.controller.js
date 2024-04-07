6f0fc2623dd338048d05d14f86f15a44f03df576
kavalon111 <ameyakavalon111@gmail.com>
2024-04-08 02:39:58 +0530

    updatevheader component with user data

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -79,7 +79,7 @@ export const google = async (req, res, next) => {
       const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
       const newUser = new User({
         username:
-          name.toLowerCase().split("").join("") +
+          name.toLowerCase().split(' ').join('') +
           Math.random().toString(9).slice(-4),
         email,
         password: hashedPassword,
