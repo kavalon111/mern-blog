6e8e63cc8bfcfd70dcf7a885141f5a34c3c8a84c
kavalon111 <ameyakavalon111@gmail.com>
2024-04-12 00:27:41 +0530

    add update user api route

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser"; 
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -17,6 +18,7 @@ mongoose
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("server is running on post 3000");
