dde3d4891461628671c1931cd2e59cd613fdf124
kavalon111 <ameyakavalon@gmail.com>
2024-04-22 03:07:14 +0530

    complete search page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,7 @@ import CreatePost from "./pages/CreatePost";
 import UpdatePost from './pages/UpdatePost';
 import PostPage from './pages/PostPage';
 import ScrollToTop from './components/ScrollToTop';
+import Search from './pages/Search';
 
 
 export default function App() {
@@ -36,6 +37,7 @@ function AppRoutes() {
       <Route path="/about" element={<About />} />
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
+      <Route path='/search' element={<Search />} />
 
       <Route element={<PrivateRoute />}>
         <Route path="/dashboard" element={<Dashboard />} />
