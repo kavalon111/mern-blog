bf32602157193bdfcaa24f5476595c7f118de571
kavalon111 <ameyakavalon@gmail.com>
2024-04-22 01:18:53 +0530

    Complete post page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,6 +14,7 @@ import PrivateRoute from "./components/PrivateRoute";
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
 import CreatePost from "./pages/CreatePost";
 import UpdatePost from './pages/UpdatePost';
+import PostPage from './pages/PostPage';
 
 export default function App() {
   return (
@@ -44,6 +45,8 @@ function AppRoutes() {
       </Route>
 
       <Route path="/project" element={<Project />} />
+      <Route path='/post/:postSlug' element={<PostPage />} />
+
     </Routes>
   );
 }
