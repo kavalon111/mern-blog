89a39e82bfabdfd7f32f0a34722ab18e54102210
kavalon111 <ameyakavalon@gmail.com>
2024-04-22 01:27:14 +0530

    add call to action to the post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
-
+import CallToAction from '../components/CallToAction';
 export default function PostPage() {
   const { postSlug } = useParams();
   const [loading, setLoading] = useState(true);
@@ -51,6 +51,9 @@ export default function PostPage() {
     </div>
     <div className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html: post && post.content}}>
 
+    </div>
+    <div className="max-w-4xl mx-auto w-full">
+        <CallToAction />
     </div>
   </main>;
 }
